f4d82aa9edfb4d0e68d103cfc1b5da78b629818f
ibrahim Tuna <tuna.cort52@gmail.com>
2024-07-24 11:36:26 +0300

    navbarlar ayrıldı ve dosyalama yapıldı

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import HomeLayout from '../component/HomeLayout'
+
+const Home = () => {
+  return (
+    <>
+    <HomeLayout/>
+    <div>Home</div>
+    </>
+    
+  )
+}
+
+export default Home
